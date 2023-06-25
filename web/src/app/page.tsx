eeff89e37b818353f1705d3bfe9ba71f9ded99f8
Clesiano Araújo Rodrigues <clesiorodrigues96@gmail.com>
2023-06-25 13:44:26 -0300

    Primeiros passos do web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <h1 className="title text-lg text-purple-600 text-center m-8" >Olá Mundo!</h1>
+    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+      <h1 className="text-4xl font-bold">Sua capsulça do termpo</h1>
+    </div>
   )
 }
