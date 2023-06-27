3fd9e2daa03cce23466b173338a0453abd798689
Clesiano Araújo Rodrigues <clesiorodrigues96@gmail.com>
2023-06-26 21:07:23 -0300

    Primeira aula finalizada

---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='bg-gray-950 flex-1 justify-center items-center'>
+      <Text className='text-gray-50 font-bold text-5xl' >Helo Word</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
