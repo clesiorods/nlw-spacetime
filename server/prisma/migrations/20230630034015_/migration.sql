e44baf1a951905b30f416524de76288d0cbfa0e6
Clesiano Araújo Rodrigues <clesiorodrigues96@gmail.com>
2023-06-30 00:42:28 -0300

    Teste de código

--- server/prisma/migrations/20230630034015_/migration.sql
+++ server/prisma/migrations/20230630034015_/migration.sql
@@ -1,13 +1,11 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Memorys` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Memorys";
-PRAGMA foreign_keys=on;
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "githubId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "avatarUrl" TEXT NOT NULL
+);
 
 -- CreateTable
 CREATE TABLE "Memory" (
@@ -19,3 +17,6 @@ CREATE TABLE "Memory" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT "Memory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -5,27 +5,7 @@ import { prisma } from "../lib/prisma";
 export async function memoriesRoutes(app: FastifyInstance) {
 
     app.get('/memories', async () => {
-        const memories = await prisma.userteste.findMany();
+        const memories = await prisma.memory.findMany();
         return memories;
-    });
-
-
-    app.get('/memories/:id', async () => {
-
-    });
-
-
-    app.post('/memories', async () => {
-
-    });
-
-
-    app.put('/memories/:id', async () => {
-
-    });
-
-
-    app.delete('/memories/:id', async () => {
-
     });
 }
